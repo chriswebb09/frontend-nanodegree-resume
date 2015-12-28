27cd2e5f692ee3bfb21560f4cedf23efbc912ff6
Christopher Webb-Orenstein <ChrisW@ChristosMacBook.home>
2015-12-28 02:04:52 -0500

    updating project

---
+++ js/helper.js
@@ -0,0 +1,48 @@
+var HTMLheaderName = '<h2 class="header-name">%data%';
+var HTMLheaderRole = '<span> %data%</span></h2>';
+
+var HTMLcontactGeneric = '<li class="contact-item"><span class="contact-title">%contact%</span><span class="contact-data">%data%</span></li>';
+var HTMLmobile = '<li class="contact-item"><span class="contact-title">mobile</span><span class="contact-data">%data%</span></li>';
+var HTMLemail = '<li class="contact-item"><span class="contact-title">email</span><span class="contact-data">%data%</span></li>';
+var HTMLtwitter = '<li class="contact-item"><span class="contact-title">twitter</span><span class="contact-data">%data%</span></li>';
+var HTMLgithub = '<li class="contact-item"><span class="contact-title">github</span><span class="contact-data">%data%</span></li>';
+var HTMLblog = '<li class="contact-item"><span class="contact-title">blog</span><span class="contact-data">%data%</span></li>';
+var HTMLlocation = '<li class="contact-item"><span class="contact-title">location</span><span class="contact-data">%data%</span></li>';
+var HTMLlinkedin = '<li class="contact-item"><span class="contact-title">linkedin</span><span class="contact-data">%data%</span></li>';
+var HTMLfacebook = '<li class="contact-item"><span class="contact-title">facebook</span><span class="contact-data">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic img-rounded img-thumbnail">';
+var HTMLwelcomeMsg = '<h3 id="welcome-message">%data%</h3>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills"></ul>';
+var HTMLskills = '<li class="skill-item">%data%</li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<h3 class="entry-title" href="#">%data%';
+var HTMLworkTitle = ' - %data%</h3>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<h3>%data%</3>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p>%data%</p>';
+var HTMLprojectImage = '<img src="%data%" class="project-img img-rounded img-thumbnail">';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em>Major: %data%</em>';
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineSchool = '<a href="#">%data%';
+var HTMLonlineCourse = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<a href="#">%data%</a>';
+
+var internationalizeButton = '<button>Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
